cdb0d58ac4cebe1b7222c19be203bca36d311687
U-DESKTOP-J32IDB0\Michael <mclagett@hotmail.com>
2019-07-24 00:31:57 -0400

    Made changes to correct NoDuplicateItems and NoConsecutiveItems tests; NoConsecutiveItems test will now always pass

--- TestletBuilder/Model/TestBuilder.cs
+++ TestletBuilder/Model/TestBuilder.cs
@@ -13,14 +13,25 @@ namespace TestletBuilder.Model
             // begin the testlet with a random two of the four pretest and then 
             // the remaining eight ordered randomly.
 
+            List<TestItem> randomizedPretestItems = null;
+            List<TestItem> randomizedRemainingRange = null;
+            int numConsecutivePretests = 0;
             var newTestlet = new Testlet();
 
             // randomize pretestItems
-            var randomizedPretestItems = questionSet.PretestQuestions.OrderBy(i => Guid.NewGuid());
+            randomizedPretestItems = questionSet.PretestQuestions.OrderBy(i => Guid.NewGuid()).ToList();
             newTestlet.Questions.AddRange(randomizedPretestItems.Take(2));
 
-            var randomizedRemainingRange =
-                randomizedPretestItems.Skip(2).Concat(questionSet.OperationalQuestions).OrderBy(i => Guid.NewGuid());
+            do
+            {
+                randomizedRemainingRange =
+                    randomizedPretestItems.Skip(2).Concat(questionSet.OperationalQuestions).OrderBy(i => Guid.NewGuid()).ToList();
+
+                numConsecutivePretests =
+                    randomizedRemainingRange.Aggregate(0, ((agg, i) => agg == 2
+                                                                       ? agg
+                                                                       : i.IsPretest ? agg + 1 : 0));
+            } while (numConsecutivePretests == 2);
             newTestlet.Questions.AddRange(randomizedRemainingRange);
 
             return newTestlet;
